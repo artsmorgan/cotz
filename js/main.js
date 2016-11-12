6cbaf3e1767dc3a38efbe7fec9415e8a270ef7c5
Walter Calderon <walcalde@crsjopgp0083.global.publicisgroupe.net>
2016-11-11 18:08:50 -0600

    update list transactions layout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 
+(function ($){
+  $(document).ready(function(){
+    var $productForm = $('.row-product').clone();
+
+    $('.row-foot .btn').click(function(){
+      var $product = $('.row-product:last'),
+          $contentCollapse = $product.find('.content-collapse');
+
+      if($contentCollapse.is(':visible')){
+        $contentCollapse.slideUp();
+        $product.find('.show-collapse').toggle();
+        $product.find('.hide-collapse').toggle();
+      }
+
+      $product.after($productForm.clone());
+    });
+
+    $('.transactions-list').on('click', '.btns-collapse a', function(e){
+      e.preventDefault();
+      var $product = $(this).closest('.row-product'),
+          $contentCollapse = $product.find('.content-collapse');
+
+      $contentCollapse.slideToggle();
+      $product.find('.show-collapse').toggle();
+      $product.find('.hide-collapse').toggle();
+    });
+  });
+
+})(jQuery);
