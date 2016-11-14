f289b16e2da778b4d37bd97ebb2032e974092fcc
Walter Calderon <walcalde@crsjopgp0083.global.publicisgroupe.net>
2016-11-14 08:47:42 -0600

    Move client info to modal

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 
 (function ($){
   $(document).ready(function(){
-    var $productForm = $('.row-product').clone();
+    var $productForm = $('.row-product').clone().addClass('disp--hide');
 
     $('.row-foot .btn').click(function(){
       var $product = $('.row-product:last'),
-          $contentCollapse = $product.find('.content-collapse');
+          $contentCollapse = $product.find('.content-collapse'),
+          $newProduct = $productForm.clone();
 
       if($contentCollapse.is(':visible')){
         $contentCollapse.slideUp();
@@ -13,7 +14,8 @@
         $product.find('.hide-collapse').toggle();
       }
 
-      $(this).closest('.row-foot').before($productForm.clone());
+      $(this).closest('.row-foot').before($newProduct);
+      $newProduct.slideDown();
     });
 
     $('.transactions-list').on('click', '.btns-collapse a', function(e){
@@ -34,11 +36,33 @@
       $modal.find('.modal-body').text('Seguro que desea eliminar este item?');
       $modal.find('.btn-primary').text('Eliminar');
 
-
       $modal.modal({ backdrop: 'static', keyboard: false })
-        .one('click', '#action-exc', function (e) {
-            $item.remove();
+        .one('click', '.modal-footer button', function (e) {
+            if( $(this).is('#action-exc') ){
+                $item.slideUp(400, function(){
+                    $item.remove();
+                });
+            }
+
+            $('.modal-footer button').off('click');
         });
+    });
+
+    $('.info-cliente button').on('click', function(){
+      $('#clienteModal').modal({ backdrop: 'static', keyboard: false });
+    });
+
+    $('[data-bind]').each(function(){
+      var $this = $(this),
+          $bind = $($this.data('bind'));
+
+      $this.on('keyup', function(){
+        $bind.val($this.val());
+      });
+
+      $bind.on('keyup', function(){
+        $this.val($bind.val());
+      });
     });
 
   });
