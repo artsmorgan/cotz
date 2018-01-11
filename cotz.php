d9c779b848ec6a93ef1aa9ef0f7fc4298113b79c
amorgan <amorgan@xtensifi.com>
2018-01-11 11:28:45 -0600

    inv change

--- cotz.php
+++ cotz.php
@@ -57,7 +57,7 @@ $userdata = dbAdmin::getInstancia()->getAllFromUserByUsername($username);
             <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Consultar Inventario</h4>
+                <h4 class="modal-title" id="myModalLabel">Consultar Inventario. [updating]</h4>
               </div>
               <div class="modal-body">
                 <table id="table" data-pagination="true"
