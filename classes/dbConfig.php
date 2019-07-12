3a9f613ecc563c5f2ce9057f9d49973f6a8c00e2
alex <amorgan115@gmail.com>
2019-07-12 12:46:45 -0600

    staging modification

--- classes/dbConfig.php
+++ classes/dbConfig.php
@@ -7,47 +7,53 @@
  */
 class dbConfig {
 
-	$this->$env = 'local';
-    $this->$svr = $_SERVER['SERVER_NAME'];
+	function __construct(){
+		
+			$isLocal = false;
+		    $env = 'local';
+		    $svr = $_SERVER['SERVER_NAME'];
 
-    $this->$localKnownHosts = array(
-        'crm.local',
-        'tecnosagot.crm.local'
-    );
-    $this->$stageKnownHosts = array(
-        'zurmojames.com',
-        'demo-ts.united-crm.com'
-    );
-    $this->$prodKnownHosts = array(
-        'tecnosagot.united-crm.com'
-    );
-    if(in_array($this->$svr, $this->$localKnownHosts)){
-        $this->$isLocal = true;
-        $this->$env = 'local';
-    }else if(in_array($this->$svr, $this->$stageKnownHosts)){
-        $this->$isLocal = false;
-        $this->$env = 'stage';
-    }else if(in_array($this->$svr, $this->$prodKnownHosts)){
-        $this->$isLocal = false;
-        $this->$env = 'prod';
-    }else{
-        //Not known environmet
-        die('not known environment');
-    }
+		    $localKnownHosts = array(
+		        'crm.local',
+		        'tecnosagot.crm.local'
+		    );
+		    $stageKnownHosts = array(
+		        'zurmojames.com',
+		        'demo-ts.united-crm.com'
+		    );
+		    $prodKnownHosts = array(
+		        'tecnosagot.united-crm.com'
+		    );
+		    if(in_array($svr, $localKnownHosts)){
+		        $isLocal = true;
+		        $env = 'local';
+		    }else if(in_array($svr, $stageKnownHosts)){
+		        $isLocal = false;
+		        $env = 'stage';
+		    }else if(in_array($svr, $prodKnownHosts)){
+		        $isLocal = false;
+		        $env = 'prod';
+		    }else{
+		        //Not known environmet
+		        die('not known environment');
+		    }
 
-    define('ENV', $this->$env);
+		    define('ENV', $env);
 
 
-    $this->$hostInfo = 'http://'.$this->$svr;
+		    $hostInfo = 'http://'.$svr;
 
-    $this->$dbAccess['prod']['username'] = 'root';
-    $this->$dbAccess['prod']['password'] = 'rootatlocalhost';
+		    $this->$dbAccess['prod']['username'] = 'root';
+		    $this->$dbAccess['prod']['password'] = 'rootatlocalhost';
 
-    $this->$dbAccess['stage']['username'] = 'zurmouser';
-    $this->$dbAccess['stage']['password'] = 'kalika@123';
+		    $this->$dbAccess['stage']['username'] = 'zurmouser';
+		    $this->$dbAccess['stage']['password'] = 'kalika@123';
 
-    $this->$dbAccess['local']['username'] = 'root';
-    $this->$dbAccess['local']['password'] = 'rootatlocalhost';
+		    $this->$dbAccess['local']['username'] = 'root';
+		    $this->$dbAccess['local']['password'] = 'rootatlocalhost';
+		}
+
+
     
     public static $db_conn_server = "localhost";
     public static $db_conn_user = $this->$dbAccess[$env]['username'];
