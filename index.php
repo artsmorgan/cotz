470716a7f06498e45daff882a28a7735a9e81758
artsmorgan <amorgan115@gmail.com>
2019-10-14 04:01:55 +0000

    new changes in prod

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ include_once (AS_PATH.'/classes/dbAdmin.php');
 
 // $path = "'".PATH."'";
 $path = "'"."http://".$_SERVER['SERVER_NAME']."'";
+
+// echo '$path ----> ' . $path; die();
+
 $username = $_GET['u'];
 $usernameToString = "'".$username."'";
 
